2ac297b2f1846f9e658404d3f18e40a18cd55a13
Harsh Tyagi <your-email@example.com>
2026-07-26 16:56:28 +0530

    update backend url

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
+        destination: "https://namankartbackend.onrender.com/api/:path*",
       },
     ];
   },
